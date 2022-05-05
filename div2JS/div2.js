2664a79e0b4dc019f2700a4bfb450f6cd3e0a635
alupoaiemircea <alupoaiemircea@gmail.com>
2022-05-05 20:19:49 +0300

    div2

--- div2JS/div2.js
+++ div2JS/div2.js
@@ -13,3 +13,20 @@ function togglePanel() {
     divElement.classList.remove("collapsed");
   }
 }
+
+
+function toggleRentFormOn() {
+  
+  var divElement = document.querySelector('.rent_form_container');
+
+  divElement.classList.remove("hidden"); 
+    divElement.classList.add("expanded"); 
+}
+
+function toggleRentFormOff() {
+  
+  var divElement = document.querySelector('.rent_form_container');
+
+  divElement.classList.remove("expanded"); 
+    divElement.classList.add("hidden"); 
+}
