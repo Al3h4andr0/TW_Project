85de3b834ed9b031ef7bbd6bcfd781509b29424f
alupoaiemircea <alupoaiemircea@gmail.com>
2022-06-13 13:32:17 +0300

    register done

--- public/temp/api/register.js
+++ public/temp/api/register.js
@@ -1,21 +1,30 @@
 
 
-function register(){
+async function register(){
    
-    // const rootAPI = `http://localhost:8000/api/register`;
+    const rootAPI = `http://localhost:8000/api/register`;
     
-    // const form=document.getElementById('register_form');
+    
+    let body={
+        username:document.getElementById('username').value,
+        password:document.getElementById('password').value
+    }
+    try {
+        let register =  await fetch(rootAPI, {method: 'POST', body: JSON.stringify(body), headers:{'Content-Type': 'application/json'}});
+        console.log(register);
+    } catch (e) {
+        throw e;
+    } finally{
+        document.getElementById('username').value='';
+        document.getElementById('password').value='';
+      const form= document.querySelector('.register_form_container');
+      form.classList.remove('expanded');
+      form.classList.add('hidden');
+        console.log(body);
+    
+    }
 
-    // form.addEventListener("submit", e =>{
-    //     e.preventDefault();
-    //     const data = new FormData(form);
-    //     for (const [name,value] of data) {
-    //       console.log(name, ":", value)
-    //     }
-    //   })
-    console.log("aASDAS")
-    document.getElementById('username').innerHTML='';
-    document.getElementById('password').innerHTML='';
+    
 }
 
 document.getElementById('register').addEventListener("click",register);
