d296482790fc5f1e33b28976702b6048b57466f7
Al3h4andr0 <76854850+Al3h4andr0@users.noreply.github.com>
2022-06-17 14:56:16 +0300

    changes to login.js

--- public/temp/api/login.js
+++ public/temp/api/login.js
@@ -10,7 +10,7 @@ async function login(){
 
     fetch(rootAPI, {method: 'POST', body: JSON.stringify(body), headers:{'Content-Type': 'application/json'}})
     .then((res) => {
-        if (res.status === 404) {
+        if (res.status === 400) {
             alert("username or password incorrect brother");
         }else
         if(res.status >=200 && res.status <=399){
@@ -18,7 +18,7 @@ async function login(){
       const form= document.querySelector('.login_form_container');
         form.classList.remove('expanded');
         form.classList.add('hidden');
-       // asteaptaPing(); // sau ping()?
+        asteaptaPing(); // sau ping()?
     }
     })
     .catch(ex => {
@@ -29,32 +29,33 @@ async function login(){
         document.getElementById('password_for_login').value='';
 }
 
-// async function ping(){
-//     const rootAPI = 'http://localhost:8000/api/ping';
-//     fetch(rootAPI,  {method: 'GET', headers:{'Content-Type': 'application/json'}})
-//     .then((res)=> {
-//         if(res.status === 401){
-//         console.log("trb login");
-//         return 1;
-//         }
-//     return 0;
-//     })
-//     .catch(ex => {throw "eroare bossule" + ex;});
-//     console.log("gata ping bro");
-// }
+async function ping(){
+    console.log("ping started");
+    const rootAPI = 'http://localhost:8000/api/ping';
+    fetch(rootAPI,  {method: 'GET', headers:{'Content-Type': 'application/json'}})
+    .then((res)=> {
+        if(res.status === 401){
+        console.log("trb login");
+        return 1;
+        }
+    return 0;
+    })
+    .catch(ex => {throw "eroare bossule" + ex;});
+    console.log("gata ping bro");
+}
 
 // setInterval(ping,5000);
 
-// function asteaptaPing(){
-//     var interval = setInterval(async function(){
-//         var counter = 0;
-//         if(ping() != 0)
-//         counter=1;
-//         if(counter == 1)
-//         return;
-//     }, 5000)
+function asteaptaPing(){
+    var interval = setInterval(async function(){
+        var counter = 0;
+        if(ping() != 0)
+        counter=1;
+        if(counter == 1)
+        return;
+    }, 5000)
     
-// }
+}
 
 document.getElementById('login').addEventListener("click",login);
 
