3640a05704353d828b97ef89161be781f1915ac7
Bogdanf21 <79158552+Bogdanf21@users.noreply.github.com>
2022-06-18 17:35:45 +0300

    added geolocation API to add apartments

--- public/temp/api/addApartment.js
+++ public/temp/api/addApartment.js
@@ -1,14 +1,30 @@
 const rootAPI = `http://localhost:8000/api/locations/upload`;
+const provider = new GeoSearch.OpenStreetMapProvider();
+
 
 const addApartmentListenerInit = () => {
     const myForm = document.getElementById("add_apartment_form");
-    myForm.addEventListener("submit", e => {
+    myForm.addEventListener("submit", async (e) => {
         e.preventDefault();
-        
-    
         const formData = new FormData(myForm);
-       fetch(rootAPI, {method: 'POST', body: formData});
-    })
+        const results = await provider.search({query:formData.get("address")});
+        console.log(results);
+        var lat;
+        var lng;
+        if(results.length === 0)
+        console.log("nothing found");
+        else
+        {
+            lat = results[0].y;
+            lng = results[0].x;
+        }
+            formData.append("lat", lat);
+            formData.append("lng", lng);
+        
+
+        fetch(rootAPI, {method: 'POST', body: formData});
+        }
+    )
 
 }
 
